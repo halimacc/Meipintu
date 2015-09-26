cb48cee3b2b46510f49fe2560f0218f91a413f5b
Chong <conradhe@qq.com>
2015-09-26 12:12:51 +0800

    first dagger2 module & example drawer activity

---
+++ meipintu/src/main/java/me/chong/meipintu/MeipintuComponent.java
@@ -0,0 +1,13 @@
+package me.chong.meipintu;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by Chong on 2015/9/26.
+ */
+@Singleton
+@Component(modules = MeipintuModule.class)
+public interface MeipintuComponent {
+}
---
+++ meipintu/src/main/java/me/chong/meipintu/ui/UiModule.java
@@ -0,0 +1,10 @@
+package me.chong.meipintu.ui;
+
+import dagger.Module;
+
+/**
+ * Created by Chong on 2015/9/26.
+ */
+@Module
+public class UiModule {
+}
