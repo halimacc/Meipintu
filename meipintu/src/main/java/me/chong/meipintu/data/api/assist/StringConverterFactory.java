7056a11c815ca814f076056dd8628ef4629b2215
Chong <conradhe@qq.com>
2015-10-05 23:34:34 +0800

    data encoding problem fixed

--- meipintu/src/main/java/me/chong/meipintu/data/api/assist/StringConverterFactory.java
+++ meipintu/src/main/java/me/chong/meipintu/data/api/assist/StringConverterFactory.java
@@ -17,7 +17,8 @@ import retrofit.Converter;
  * Created by Chong on 2015/10/5.
  */
 public class StringConverterFactory extends Converter.Factory {
-    private StringConverterFactory() {}
+    private StringConverterFactory() {
+    }
 
     public static StringConverterFactory create() {
         return new StringConverterFactory();
@@ -31,7 +32,8 @@ public class StringConverterFactory extends Converter.Factory {
         return null;
     }
 
-    @Override public Converter<?, RequestBody> toRequestBody(Type type, Annotation[] annotations) {
+    @Override
+    public Converter<?, RequestBody> toRequestBody(Type type, Annotation[] annotations) {
         return null;
     }
 
@@ -39,22 +41,7 @@ public class StringConverterFactory extends Converter.Factory {
 
         @Override
         public String convert(ResponseBody value) throws IOException {
-            long contentLength = value.contentLength();
-            if (contentLength < 0) {
-                contentLength = value.bytes().length;
-            }
-            int bufferSize = 4096;
-            char[] buffer = new char[bufferSize];
-            int now = 0;
-
-            StringBuilder builder = new StringBuilder();
-            while (now < contentLength) {
-                int read = value.charStream().read(buffer, now, bufferSize);
-                builder.append(buffer, 0, read);
-                now += read;
-            }
-
-            return builder.toString();
+            return new String(value.bytes(), "UTF-8");
         }
     }
 }
